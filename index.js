46048b762da533bb98266b5349c1d0e6ae619d6e
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-08-21 22:04:20 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// //Code your solution here
+// function getTotalAmountForProducts(batteryBatches) {
+//   let totalPrice = 0;
+
+//   for (const product of batteryBatches) {
+//     totalPrice += product.price;
+//   };
+
+//   return totalPrice;
+// }
+
+// console.log(getTotalAmountForProducts(batteryBatches));
+
+// Sum of all the assembled batteries
+let totalBatteries = batteryBatches.reduce( (batteryAmount, assembledBatteries) => { return batteryAmount + assembledBatteries })
